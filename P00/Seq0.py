773db313eb1284cb054d0095dac6d2945a6b6f8f
pablogonzalezl <p.gonzalezl.2023@alumnos.urjc.es>
2024-02-13 16:39:59 +0100

    This is going to be my first but not last commit

--- P00/Seq0.py
+++ P00/Seq0.py
@@ -1,4 +1,85 @@
 ## Practice 0
-
+#1
 def seq_ping():
     print('OK')
+
+#2
+def seq_read_fasta(filename):
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            seq_joined = ''.join(lines[1:])
+            sequence = seq_joined[:20]
+    except FileNotFoundError:
+        print("Error: File", filename, "not found.")
+        sequence = None
+    return sequence
+
+# 3:
+def seq_len(seq):
+    sequence = ''
+    with open(seq, 'r') as f:
+        header = next(f)
+        for line in f:
+            sequence += line.strip('\n')
+    return len(sequence)
+
+# 4:
+def seq_count_base(seq, base):
+    try:
+        with open(seq, 'r') as file:
+            lines = file.readlines()
+            seq_joined = ''.join(lines[1:])
+    except FileNotFoundError:
+        print("Error: File", seq, "not found.")
+        count_base = None
+    else:
+        count_base = 0
+        for i in seq_joined:
+            if i == base.upper():
+                count_base += 1
+            else:
+                pass
+    return count_base
+
+# 5:
+
+
+def seq_count(seq):
+    try:
+        with open(seq, 'r') as file:
+            lines = file.readlines()
+            seq_joined = ''.join(lines[1:])
+    except FileNotFoundError:
+        print("Error: File", seq, "not found.")
+        dic_bases = {}
+    else:
+        dic_bases = {'A': 0, 'G': 0, 'T': 0, 'C': 0}
+        for i in seq_joined:
+            if i == 'A':
+                dic_bases['A'] += 1
+            elif i == 'T':
+                dic_bases['T'] += 1
+            elif i == 'C':
+                dic_bases['C'] += 1
+            elif i == 'G':
+                dic_bases['G'] += 1
+    return dic_bases
+
+# 6:
+
+
+def seq_reverse(seq, n):
+    try:
+        with open(seq, 'r') as file:
+            lines = file.readlines()
+            seq_joined = ''.join(lines[1:])
+            sequence = seq_joined[:n]
+    except FileNotFoundError:
+        print("Error: File", seq, "not found.")
+        sequence = None
+    return sequence
+
+#7
+
+def seq_complement(seq):
