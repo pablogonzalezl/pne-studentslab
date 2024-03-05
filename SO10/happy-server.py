2fe0e067eb604f9a79d123801ace0c68e5cbdbbb
pablogonzalezl <p.gonzalezl.2023@alumnos.urjc.es>
2024-03-05 16:37:28 +0100

    This is going to be my first but not last commit

---
+++ SO10/happy-server.py
@@ -0,0 +1,19 @@
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "127.0.0.1" # this IP address is local, so only requests from the same machine are possible
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
+# -- Close the socket
+ls.close()
---
+++ SO10/notes b.py
@@ -0,0 +1,30 @@
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "127.0.0.1" # this IP address is local, so only requests from the same machine are possible
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
+while True:
+    (rs, address) = ls.accept()
+
+    print(f'Client {address}')
+
+    msg = rs.recv(2048).decode("utf-8")
+
+    print("The client says ..." +msg)
+
+    
+
+# -- Close the socket
+ls.close()
